2a12cf3b1ca42921e91016e72eeb62c812795cf7
pratyushsingha <pratyushsingha83@gmail.com>
2023-01-09 23:01:55 +0530

    pratyush commit

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,16 +1,63 @@
+<?php include('partials/menu.php'); ?>
 
-    <?php include('partials/menu.php'); ?>
+<!-----MAIN CONTENT SECTION START----->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>MANAGE ADMIN</h1>
+        <br />
+        <br />
+        <br />
+        <!-- botton to add admin -->
+        <a href="add-admin.php" class="btn-primary">Add Admin</a>
+        <br />
+        <br />
+        <br />
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>username</th>
+                <th>Actions</th>
+            </tr>
 
-    <!-----MAIN CONTENT SECTION START----->
-    <div class="main-content">
-        <div class="wrapper">
-            <h1>MANAGE ADMIN</h1>
+            <tr>
+                <td>1.</td>
+                <td>manish </td>
+                <td>devimanish </td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">delete Admin</a>
+                </td>
+            </tr>
 
-            
-            
-            <div class="clearfix"></div>
-        </div>
+            <tr>
+                <td>2.</td>
+                <td>manish </td>
+                <td>devimanish </td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">delete Admin</a>
+
+                </td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>manish </td>
+                <td>devimanish </td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">delete Admin</a>
+                </td>
+            </tr>
+        </table>
+
+
+
+
+        <div class="clearfix"></div>
     </div>
-    <!-------MAIN CONTENT SECTION END----->
+</div>
+<!-------MAIN CONTENT SECTION END----->
 
-    <?php include('partials/footer.php'); ?>
+<?php include('partials/footer.php'); ?>
